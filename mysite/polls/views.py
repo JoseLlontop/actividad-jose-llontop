f2f7ac48653792124ce5766a0bb5dcf04414cfd8
JoseLlontop <josellontop100@gmail.com>
2024-04-10 15:04:43 -0300

    [Parte 3.4] generando un views de un errror 404

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
 from django.template import loader
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
@@ -23,5 +25,6 @@ def index(request):
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
 
-
-# Leave the rest of the views (detail, results, vote) unchanged    
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
